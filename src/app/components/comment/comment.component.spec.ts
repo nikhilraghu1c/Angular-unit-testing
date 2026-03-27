304ad41cc36070ba158b4dd7a1f89ac55a23fc1b
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2026-03-27 18:28:52 +0530

    Added integration test cases for the comment post method

--- src/app/components/comment/comment.component.spec.ts
+++ src/app/components/comment/comment.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommentComponent } from './comment.component';
 import { CommentsService } from '../../services/comments.service';
-import { DebugElement } from '@angular/core';
+import { DebugElement, input } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 
@@ -66,9 +66,54 @@ describe('CommentComponent', () => {
     commentSrv.getAllComments.and.returnValue(of(mockComments));
 
     // detectChanges is used to trigger change detection in the component. It is necessary to call this method after setting up the mock response to ensure that the component's template is updated with the new data.
-    fixture.detectChanges(); 
+    fixture.detectChanges();
     expect(component.allComments.length).toBe(2);
     expect(el.queryAll(By.css('li')).length).toBe(2);
+  });
 
+  it('should throw alert if comment text is empty', () => {
+    spyOn(window, 'alert'); // Spy on the global alert function to check if it is called when the comment text is empty.
+    component.text = '';
+    component.handleSubmit();
+    expect(window.alert).toHaveBeenCalledWith('Please add a comment');
+    expect(window.alert).toHaveBeenCalledTimes(1);
+    expect(commentSrv.postComment).not.toHaveBeenCalled(); // Check that the postComment method of the comment service was not called when the comment text is empty.
+  });
+
+  it('should add comment on UI when user types and click on post button', () => {
+    // Setting up the mock response for the getAllComments method of comment service to return an observable of the mockComments array when it is called during the component's initialization (ngOnInit). and.returnValue is used to specify the value that should be returned when the getAllComments method of the commentSrv spy is called.
+    const mockComments = [
+      { id: 1, text: 'comment 1' },
+      { id: 2, text: 'comment 2' },
+    ];
+    commentSrv.getAllComments.and.returnValue(of(mockComments));
+    fixture.detectChanges();
+
+    // Setting up the mock response for the postComment method of comment service to return an observable of the new comment when it is called during the handleSubmit method. and.returnValue is used to specify the value that should be returned when the postComment method of the commentSrv spy is called.
+    const newComment = { id: 3, text: 'Nice pic' };
+    commentSrv.postComment.and.returnValue(of(newComment));
+    spyOn(Date, 'now').and.returnValue(3);
+
+    // simulate user typing a comment
+    const inputEl: HTMLInputElement =
+      fixture.nativeElement.querySelector('Input');
+    inputEl.value = newComment.text;
+    inputEl.dispatchEvent(new Event('input')); // Manually trigger the input event to update the component's text property with the new value.
+
+    const buttonEl: HTMLButtonElement =
+      fixture.nativeElement.querySelector('button');
+    buttonEl.click(); // Simulate a click event on the post button to trigger the handleSubmit method of the component.
+    
+    fixture.detectChanges(); // Trigger change detection to update the component's template with the new comment.
+    const commentEl = el.queryAll(By.css('li')); 
+    expect(commentEl.length).toBe(3);
+    expect(commentEl[2].nativeElement.innerText).toBe(newComment.text);
+    expect(commentSrv.postComment).toHaveBeenCalledWith({ id: 3, text: 'Nice pic' }); // Check that the postComment payload was correct or not.
   });
 });
+
+/* can we use spyOn for any other method of the component? Yes, we can use spyOn to spy on any method of the component. For example, we can spy on the loadComments method to check if it is called when a new comment is posted successfully. We can do this by adding the following code in the test case for posting a comment:
+spyOn(component, 'loadComments'); // Spy on the loadComments method of the component
+component.handleSubmit(); // Call the handleSubmit method to post a comment
+expect(component.loadComments).toHaveBeenCalled(); // Check if the loadComments method was called after posting a comment successfully
+it will fail we are not calling the loadcomments method in the handleSubmit method. */
