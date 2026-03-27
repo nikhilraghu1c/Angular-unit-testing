01dc34e8f2db13f42fc0893532d3623815e78f62
Nikhil Raghuwanshi <nikhilofficial557@gmail.com>
2026-03-27 20:21:02 +0530

    Added comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,3 +14,9 @@ import { NavComponent } from './components/nav/nav.component';
 export class AppComponent {
   title = 'test-new-prep';
 }
+
+/**
+ * To generate code coverage , use command  ng test --no-watch --code-coverage
+ * After running the above command, a new folder named coverage will be created in the root directory of the project. Inside the coverage folder, there will be an index.html file that you can open in a web browser to view the code coverage report.
+ * --no-watch flag is used to run the tests only once and then exit, instead of keeping the test runner active and watching for file changes. This is useful when you want to generate a code coverage report without continuously running the tests.
+ */
